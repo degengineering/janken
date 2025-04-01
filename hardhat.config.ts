8669e46dbd52619687d1b3efa990a41f70487571
gbdevw <guillaume.braibant@lake42.eu>
2025-04-01 23:20:42 +0200

    Fix compile error with  viaIR option

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,16 @@ import "@typechain/hardhat";            // <-- for generating types
 //import "@typechain/viem";              // <-- viem target
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  }
 };
 
 export default config;
